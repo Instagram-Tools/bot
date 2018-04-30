0d92f29f291030e680f4a1d6ff61ceff8da7bafc
Gott50 <xxGott50xx@gmail.com>
2018-04-30 10:17:05 +0200

    use Bot

--- quickstart.py
+++ quickstart.py
@@ -1,10 +1,4 @@
-import os
-import time
-from tempfile import gettempdir
-
-from selenium.common.exceptions import NoSuchElementException
-
-from instapy import InstaPy
+from include import Bot
 from env import insta_username,insta_password
 
 # set headless_browser=True if you want to run InstaPy on a server
@@ -14,30 +8,23 @@ from env import insta_username,insta_password
 #   Settings.database_location = '/path/to/instapy.db'
 #   Settings.chromedriver_location = '/path/to/chromedriver'
 
-session = InstaPy(username=insta_username,
+bot = Bot(username=insta_username,
                   password=insta_password,
                   headless_browser=False,
                   multi_logs=True)
 
 try:
-    session.login()
+    bot.login()
 
-    # settings
-    session.set_upper_follower_count(limit=2500)
-    session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-    session.set_dont_include(['friend1', 'friend2', 'friend3'])
-    session.set_dont_like(['pizza', 'girl'])
-
-    # actions
-    session.like_by_tags(['natgeo'], amount=1)
+    bot.settings()
+    bot.act()
 
 except Exception as exc:
     # if changes to IG layout, upload the file to help us locate the change
     if isinstance(exc, NoSuchElementException):
         file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
         with open(file_path, 'wb') as fp:
-            fp.write(session.browser.page_source.encode('utf8'))
+            fp.write(bot.browser.page_source.encode('utf8'))
         print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
             '*' * 70, file_path))
     # full stacktrace when raising Github issue
@@ -45,4 +32,4 @@ except Exception as exc:
 
 finally:
     # end the bot session
-    session.end()
+    bot.end()
