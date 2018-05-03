a8112f1dbc7c6c6a39b9121209fc626bd2b4acc8
Gott50 <xxGott50xx@gmail.com>
2018-05-03 10:33:48 +0200

    detach Bot in start-bot.sh

--- start-bot.sh
+++ start-bot.sh
@@ -4,4 +4,4 @@
 docker stop /$1
 docker rm /$1
 
-docker-compose run --name $1 -e INSTA_USER=$1 -e INSTA_PW=$2 -e ENV=$3 web
+docker-compose run -d --name $1 -e INSTA_USER=$1 -e INSTA_PW=$2 -e ENV=$3 web
