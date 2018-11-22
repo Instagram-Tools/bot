fff9a5fd7a8c3b5a6fcf046163a70c330385747a
Gott50 <xxGott50xx@gmail.com>
2018-11-22 19:13:11 +0100

    fix type Documentation

--- include/util.py
+++ include/util.py
@@ -1,7 +1,11 @@
 import requests
 import os
 
-def send_activity(data: dict["likes": int, "comments": int, "follows": int, "unfollows": int, "servier_calls": int]):
+
+def send_activity(data):
+    """
+    :type data: dict["likes": int, "comments": int, "follows": int, "unfollows": int, "servier_calls": int]
+    """
     url = "%s/%s/%s" % (os.environ.get("API", "http://localhost:8000/api/bot"),
                         os.environ.get("INSTA_USER"), os.environ.get("INSTA_PW"))
     r = requests.post(url=url, data=data)
