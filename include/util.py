2ae723cad197016688e3ee594d47b1e64f32e3a6
Gott50 <xxGott50xx@gmail.com>
2018-11-22 18:10:59 +0100

    use default API: "http://localhost:8000/api/bot"

--- include/util.py
+++ include/util.py
@@ -2,5 +2,6 @@ import requests
 import os
 
 def send_activity(data: dict["likes": int, "comments": int, "follows": int, "unfollows": int, "servier_calls": int]):
-    url = "%s/%s/%s" % (os.environ.get("API"), os.environ.get("INSTA_USER"), os.environ.get("INSTA_PW"))
+    url = "%s/%s/%s" % (os.environ.get("API", "http://localhost:8000/api/bot"),
+                        os.environ.get("INSTA_USER"), os.environ.get("INSTA_PW"))
     r = requests.post(url=url, data=data)
