d88ffd67e349074ee5fdb6158894765c77d64427
Gott50 <xxGott50xx@gmail.com>
2018-12-14 23:49:35 +0100

    fix by ending Session before try again: ERROR [2018-12-14 22:27:31] [johannes.eisenlohr]  Excepiton in act(): Message: invalid session id       (Driver info: chromedriver=2.45.615279 (12b89733300bd268cff3b78fc76cb8f3a7cc44e5),platform=Linux 4.14.77-70.82.amzn1.x86_64 x86_64)

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -35,6 +35,7 @@ def run():
         bot.act()
     except WebDriverException as wde:
         print("WebDriverException in run(): %s \n%s" % (wde, wde.stacktrace))
+        bot.end()
         sleep(60)
         run()
 
